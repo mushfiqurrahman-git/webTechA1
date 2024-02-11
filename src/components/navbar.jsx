8c4c98d5e618966a366f77794b6b5ea70ec46b41
Mushfiqur Rahman <mymensingh456@gmail.com>
2024-02-11 14:43:45 -0500

    First commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import {ShoppingCart} from 'phosphor-react'
+import "./navbar.css";
+
+export const Navbar = () => {
+  return (
+      <div>
+        <h2>The Super Mobile Shop </h2>
+      <div className='navbar'>
+        <div className='links'>
+            <Link to="/">Shop</Link>
+            <Link to="/cart">
+                <ShoppingCart size={32}></ShoppingCart>
+            </Link>
+        </div>
+    </div>
+    </div>
+  )
+}
---
+++ src/products.js
@@ -0,0 +1,42 @@
+import product1 from "./assets/iPhoneX.jpg";
+import product2 from "./assets/iPhone11.jpg";
+import product3 from "./assets/iPhone12.jpg";
+import product4 from "./assets/iPhone13.jpg";
+import product5 from "./assets/iPhone14.jpg";
+export const PRODUCTS = [
+    {
+        id:1,
+        desc:"New Era Of Mobile Computing",
+        productName:"iPhoneX",
+        price:440.0,
+        productImage:product1,
+    },
+    {
+        id:2,
+        desc:"Next gen Camera",
+        productName:"iPhone11",
+        price:460.0,
+        productImage:product2,
+    },
+    {
+        id:3,
+        desc:"Built with new A12",
+        productName:"iPhone12",
+        price:540.0,
+        productImage:product3,
+    },
+    {
+        id:4,
+        desc:"Better than ever before",
+        productName:"iPhone13",
+        price:740.0,
+        productImage:product4,
+    },
+    {
+        id:5,
+        desc:"Stunning",
+        productName:"iPhone14",
+        price:670.0,
+        productImage:product5,
+    },
+]
